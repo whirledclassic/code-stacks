247834ee47e51e979f6e2000a693f356d9f2dabe
whirledclassic <josh.awe99@gmail.com>
2026-09-15 17:13:23 -0400

    Open PLAY-ME.htm first on Win7

--- 1-INSTALL-AND-PLAY.bat
+++ 1-INSTALL-AND-PLAY.bat
@@ -1,24 +1,21 @@
 @echo off
 cd /d "%~dp0"
 title CODE STACKS
-echo CODE STACKS
-if exist "%~dp0play.html" (set GAME=%~dp0play.html) else set GAME=%~dp0index.html
-if not exist "%GAME%" (
-  echo play.html / index.html missing. Open the INNER code-stacks-main folder.
+set GAME=
+if exist "%~dp0PLAY-ME.htm" set GAME=%~dp0PLAY-ME.htm
+if not defined GAME if exist "%~dp0play.html" set GAME=%~dp0play.html
+if not defined GAME if exist "%~dp0index.html" set GAME=%~dp0index.html
+if not defined GAME (
+  echo No game file in this folder.
   pause
   goto :eof
 )
+echo Opening %GAME%
 set BROWSER=
 if exist "%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe" set BROWSER=%LOCALAPPDATA%\Google\Chrome\Application\chrome.exe
 if not defined BROWSER if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" set BROWSER=%ProgramFiles%\Google\Chrome\Application\chrome.exe
 if not defined BROWSER if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" set BROWSER=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe
 if not defined BROWSER if exist "%ProgramFiles%\Mozilla Firefox\firefox.exe" set BROWSER=%ProgramFiles%\Mozilla Firefox\firefox.exe
-if not defined BROWSER if exist "%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe" set BROWSER=%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe
-if defined BROWSER (
-  echo Opening %GAME%
-  start "" "%BROWSER%" "%GAME%"
-) else (
-  echo No Chrome/Firefox. Do not use IE.
-  start "" "%GAME%"
-)
+if defined BROWSER (start "" "%BROWSER%" "%GAME%") else start "" "%GAME%"
+echo If the page says JavaScript is running, press Stack.
 pause
