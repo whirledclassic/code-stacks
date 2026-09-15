36caa3dd5f4e071a0f9be05954813ef42382d381
whirledclassic <josh.awe99@gmail.com>
2026-09-15 16:45:27 -0400

    INSTALL.bat launches ONECLICK after copy

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,10 +1,9 @@
 @echo off
 setlocal
 title CODE STACKS installer
-echo.
-echo  CODE STACKS  -  one-click install
-echo  Folder: %USERPROFILE%\code-stacks
-echo.
+if exist "%~dp0installer.html" start "" "%~dp0installer.html"
+echo CODE STACKS install
+echo Folder: %USERPROFILE%\code-stacks
 set TARGET=%USERPROFILE%\code-stacks
 set REPO=https://github.com/whirledclassic/code-stacks.git
 set ZIPURL=https://github.com/whirledclassic/code-stacks/archive/refs/heads/main.zip
@@ -15,7 +14,7 @@ where git >nul 2>&1
 if %errorlevel%==0 goto gitinstall
 goto zipinstall
 :gitinstall
-echo Using git...
+echo Using git
 if exist "%TARGET%\.git" (
   pushd "%TARGET%"
   git pull origin main
@@ -25,7 +24,7 @@ if exist "%TARGET%\.git" (
 )
 goto finish
 :zipinstall
-echo Git not found. Downloading zip...
+echo Git missing. Zip download.
 if exist "%ZIP%" del /f /q "%ZIP%"
 if exist "%UNPACK%" rd /s /q "%UNPACK%"
 mkdir "%UNPACK%"
@@ -39,12 +38,21 @@ if not exist "%ZIP%" (
   pause
   goto eof
 )
-if exist "%~dp0tools\unzip.vbs" cscript //nologo "%~dp0tools\unzip.vbs" "%ZIP%" "%UNPACK%"
+if exist "%~dp0tools\unzip.vbs" (
+  cscript //nologo "%~dp0tools\unzip.vbs" "%ZIP%" "%UNPACK%"
+) else (
+  echo Unzip %ZIP% yourself
+  pause
+  goto eof
+)
 if exist "%UNPACK%\code-stacks-main\index.html" xcopy /e /y /q "%UNPACK%\code-stacks-main\*" "%TARGET%\"
+if exist "%UNPACK%\index.html" xcopy /e /y /q "%UNPACK%\*" "%TARGET%\"
 :finish
-echo Creating desktop shortcut...
-if exist "%TARGET%\tools\shortcut.vbs" cscript //nologo "%TARGET%\tools\shortcut.vbs" "%TARGET%\PLAY.bat" "%USERPROFILE%\Desktop\CODE STACKS.lnk"
-echo Installed. Launching PLAY.bat
-pause
-if exist "%TARGET%\PLAY.bat" call "%TARGET%\PLAY.bat"
+if exist "%TARGET%\tools\shortcut.vbs" cscript //nologo "%TARGET%\tools\shortcut.vbs" "%TARGET%\ONECLICK.bat" "%USERPROFILE%\Desktop\CODE STACKS.lnk"
+echo Installed. Starting.
+if exist "%TARGET%\ONECLICK.bat" (
+  call "%TARGET%\ONECLICK.bat"
+) else if exist "%TARGET%\PLAY.bat" (
+  call "%TARGET%\PLAY.bat"
+)
 endlocal
