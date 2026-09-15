20c74ad1b6bb3b3222487facabf37b546c5186bf
whirledclassic <josh.awe99@gmail.com>
2026-09-15 17:05:17 -0400

    v1.33.2 1-INSTALL-AND-PLAY starts the table immediately if files are here

--- 1-INSTALL-AND-PLAY.bat
+++ 1-INSTALL-AND-PLAY.bat
@@ -1,12 +1,19 @@
 @echo off
 cd /d "%~dp0"
-if exist "%~dp0INSTALL.bat" (
-  call "%~dp0INSTALL.bat"
-) else if exist "%~dp0ONECLICK.bat" (
-  call "%~dp0ONECLICK.bat"
-) else if exist "%~dp0PLAY.bat" (
+title CODE STACKS
+
+if exist "%~dp0index.html" goto playnow
+
+echo Game files missing in this folder. Running full install.
+if exist "%~dp0INSTALL.bat" call "%~dp0INSTALL.bat"
+goto eof
+
+:playnow
+if exist "%~dp0tools\shortcut.vbs" cscript //nologo "%~dp0tools\shortcut.vbs" "%~dp01-INSTALL-AND-PLAY.bat" "%USERPROFILE%\Desktop\CODE STACKS.lnk"
+echo Starting CODE STACKS
+echo Leave this window open.
+if exist "%~dp0PLAY.bat" (
   call "%~dp0PLAY.bat"
 ) else (
-  echo Missing game files.
-  pause
+  start "" "%~dp0client.html"
 )
